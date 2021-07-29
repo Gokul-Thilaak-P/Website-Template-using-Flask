360351d4a95d4a93474dbf823b67f8c37060f209
Gokul Thilaak P <88130932+Gokul-Thilaak-P@users.noreply.github.com>
2021-07-29 15:46:00 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import datetime as dt
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def homepage():
-    return render_template("index.html")
+def home_page():
+    this_year = dt.datetime.now().strftime("%Y")
+    owner_name = "Gokul Thilaak P"
+    return render_template("index.html", year=this_year, name=owner_name)
 
 
 if __name__ == "__main__":
